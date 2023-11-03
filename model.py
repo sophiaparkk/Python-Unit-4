f5c371ae19480ccf6a8eeaef8d0d573933b498ae
Sophia Park <park.sophiakela@gmail.com>
2023-11-02 18:48:52 -0600

    project 4 complete

---
+++ model.py
@@ -0,0 +1,61 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+
+    __tablename__ = 'users' #naming table separate from class
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+
+    teams = db.relationship("Team", backref = "user", lazy = True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class Team(db.Model):
+
+    __tablename__ = "teams"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    team_name = db.Column(db.String(255), unique = True, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+    def __init__(self, team_name, user_id):
+        self.team_name = team_name
+        self.user_id = user_id
+
+class Project(db.Model):
+
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    project_name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255), nullable = True)
+    completed = db.Column(db.Boolean, default = False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+    projects = db.relationship("Team", backref = "team_project", lazy = True)
+
+    def __init__(self, project_name, description, completed, team_id):
+        self.project_name = project_name
+        self.description = description
+        self.completed = completed
+        self.team_id = team_id
+
+def connect_to_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+    with app.app_context():
+        connect_to_db(app)
+    print("Connected to db...")
